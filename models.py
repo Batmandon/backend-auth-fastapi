0422db73a3e04daeca10f311df5561b1bf448ce8
Batmandon <taukeermath@gmail.com>
2026-04-01 20:52:36 +0530

    removed e-commerce logic

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-class UserCreate(BaseModel):
+class UserRegister(BaseModel):
     name: str
     email: str
     password: str
@@ -11,37 +11,3 @@ class UserLogin(BaseModel):
 
 class UserRefresh(BaseModel):
     token: str
-    
-class SellerCreate(BaseModel):
-    name: str
-    email: str
-    password: str
-    business_name: str
-    phone: int
-    gst_no: str
-
-
-class ProductCreate(BaseModel):
-    name: str
-    description: str
-    category: str
-    price: float
-    quantity: int
-    gender: str
-
-class ProductUpdate(BaseModel):
-    name: str
-    description: str
-    category: str
-    price: float
-
-class OrderCreate(BaseModel):
-    product_id: int
-    quantity: int
-    payment_method: str
-
-class BankDetails(BaseModel):
-    user_id: int
-    bank_name: str
-    account_number: str
-    ifsc_code: str
